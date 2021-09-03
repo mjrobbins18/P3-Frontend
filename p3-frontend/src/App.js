f674a00c4c7baf91850e4cec4b872e5918c6ed0c
mjrobbins18 <maxrobbins88@gmail.com>
2021-09-02 21:22:45 -0400

    2nd commit, set up router

--- p3-frontend/src/App.js
+++ p3-frontend/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Landing from './components/Landing';
 
 function App() {
   return (
     <div >
       <h1>Hello World</h1>
+      <Landing/>
     </div>
   );
 }
---
+++ p3-frontend/src/components/Landing.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Landing(props) {
+    return (
+        <div>
+            <header>
+
+            </header>
+            <main>
+                
+            </main>
+        </div>
+    );
+}
+
+export default Landing;
