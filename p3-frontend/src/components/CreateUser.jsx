28def8502ece321803da19e0ae1bbbc5a7a27479
mjrobbins18 <maxrobbins88@gmail.com>
2021-09-03 10:53:56 -0400

    4th commit, dropdown menu

---
+++ p3-frontend/src/components/CreateUser.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CreateUser(props) {
+    return (
+        <div>
+            <h1>Create User</h1>
+        </div>
+    );
+}
+
+export default CreateUser;<h1>Create User</h1>
