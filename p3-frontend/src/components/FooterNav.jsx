ea40f5482123b4aef191ad646519992dcd25d0ec
omarevolving@gmail.com <Kingoz08x!>
2021-09-06 10:59:37 -0400

    proposals

--- p3-frontend/src/components/FooterNav.jsx
+++ p3-frontend/src/components/FooterNav.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
-
+import trophy from './Images/trophy.png'
+import profile from './Images/—Pngtree—vector edit profile icon_4102545.png'
 function FooterNav(props) {
     return (
         <div className = "footerContainer">
             <div className = "Footer" id = "Nav">
-             <Link to = '/trophies'>Trophies</Link><br></br>
-             <Link to = '/create'>Create NFT</Link><br></br>
-             <Link to = '/about'>About</Link><br></br>
-                    
+             <Link to = '/trophies'className= "footerIcons" id="trophyIcon"> <img src= {trophy} alt="Trophies"/> </Link><br></br>
+             <Link to = '/create' className= "footerIcons" id= "createIcon"><img src= "https://pixabay.com/get/g9bb1a6ab138f478f88c0723ba7c77aab2df0297252b82c17cdfd2b0c42cb21783c99498cad0940bfec7f9894580d0603_640.png" alt="Create NFT"/></Link><br></br>
+             <Link to = '/about' className= "footerIcons" id= "aboutIcon"><img src= "https://visualpharm.com/assets/454/About-595b40b65ba036ed117d42d0.svg" alt="About"/></Link><br></br>
+             <Link to = '/Login' className= "footerIcons" id= "loginIcon"><img src = {profile} alt= ""/></Link>     
         </div>
         </div>
         
--- p3-frontend/src/components/Header.jsx
+++ p3-frontend/src/components/Header.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 import Dropdown from './Dropdown';
 import SearchBar from './SearchBar';
 import rocket from './Images/rocket.png'
+import trophy from './Images/trophy.png'
 import { Link } from 'react-router-dom'
 
 function Header(props) {
 
     return (
         <div className = "Header">
-            <Link to = '/'><img className = "Logo" src = { rocket } alt = "rocket logo"/></Link>
+            <Link to = '/'><img className = "Logo" src = "https://lh3.googleusercontent.com/UiF_88MADFIHbCZp88RlGbCRDX3exmR4FOg_u4XMqwmXglbmbOFq26F3XdgTgPX4PwZ5BpaViXQ9KfljhLfZci-1NJzSc1tLL_QV=s130" alt = "rocket logo"/></Link>
             <SearchBar/>
-            <Link to = '/Login'>Login</Link>
+            <div id= "dropLog">
             <Dropdown/>
+            </div>
+            {/* added div to try to keep organized on header */}
+            
         </div>
     );
 
