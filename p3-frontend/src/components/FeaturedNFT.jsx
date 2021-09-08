739b3f523295c5ba283d94e401cdda9e7eddbefe
omarevolving@gmail.com <Kingoz08x!>
2021-09-07 20:12:49 -0400

    fullscreen adjustments

--- p3-frontend/src/components/FeaturedNFT.jsx
+++ p3-frontend/src/components/FeaturedNFT.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import FeaturedImg from './Images/FeaturedImg.jpeg';
+import Introtxt from './Introtxt';
+
 function FeaturedNFT(props) {
     return (
-        <div id= "featNFT">
-            <img src={FeaturedImg} alt="" />
+
+<div className= "featDiv"> 
+<Introtxt/>
+    <script src="https://kit.fontawesome.com/95a02bd20d.js"></script>
+        <div class="featContainer">
+            
+           <div class="card">
+           <h3> Feature of the Day!!</h3>
+             <div class="face face1">
+             
+                  <i class="fab fa-windows"></i>            
+                 <img id= "NFTfeat" src={FeaturedImg} alt="" />
+               
+             </div>
+             <div class="face face2">
+               <div class="content">
+                 <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ab repudiandae, explicabo voluptate et hic cum ratione a. Officia delectus illum perferendis maiores quia molestias vitae fugiat aspernatur alias corporis?</p>
+                 <a href="#" type="button">Read More</a>
+               </div>
+             </div>
+          </div>
+          </div>
         </div>
+        // <div >
+        //     <img class="box glowing" src={FeaturedImg} alt="" />
+        // </div>
     );
 }
 
