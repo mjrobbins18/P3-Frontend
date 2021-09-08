e25800efcb7e2e1b4102cbe06d825455fd655901
mjrobbins18 <maxrobbins88@gmail.com>
2021-09-08 17:59:22 -0400

    post route fixed

--- p3-frontend/src/App.js
+++ p3-frontend/src/App.js
@@ -14,6 +14,15 @@ const loginState = {
   isLoggedIn: false,
   buyOrLogin: "/login"
 }
+const initialNftState = {
+  name: "",
+  image_url: "",
+  description: ""
+}
+const initialUserState = {
+  username: "",
+  password: ""
+}
 
 const [searchState, setSearchState] = ("")
 const [formState, setFormState] = ("")
@@ -22,18 +31,21 @@ const [collection, setCollection] = useState([])
 const [formStateMinMax, setFormStateMinMax] = useState(initialStateMinMax)
 const [inputMinMax, setInputMinMax] = useState(initialStateMinMax)
 const [loggedIn, setLoggedIn] = useState(loginState)
+const [createNftState, setCreateNftState] = useState(initialNftState)
+const [nftFormState, setNftFormState] = useState(initialNftState)
+const [userState, setUserState] = useState(initialUserState)
+const [userFormState, setUserFormState] = useState(initialUserState)
 
-const url = "http://localhost:8000/nftmarketplace/"
-const collectUrl = "http://localhost:8000/nftmarketplace/collection"
+const url = "http://localhost:8001/nftmarketplace/"
+// const collectUrl = "http://localhost:8001/nftmarketplace/collection"
 console.log(random)
     useEffect(() => {
         fetch(url).then(res => res.json()).then(res=>setRandom(res)).catch(console.error)
     },[])
-    useEffect(() => {
-      fetch(collectUrl).then(res => res.json()).then(res=>setCollection(res)).catch(console.error)
-  },[])
-
-
+  //   useEffect(() => {
+  //     fetch(collectUrl).then(res => res.json()).then(res=>setCollection(res)).catch(console.error)
+  // },[])
+console.log(random)
   return (
     <div className = "App" >
       <DataContext.Provider value = {{ formStateMinMax,
@@ -50,7 +62,18 @@ console.log(random)
                                       searchState,
                                       setSearchState,
                                       formState,
-                                      setFormState}}>
+                                      setFormState,
+                                      userState,
+                                      setUserState,
+                                      userFormState,
+                                      setUserFormState,
+                                      createNftState,
+                                      setCreateNftState,
+                                      nftFormState,
+                                      setNftFormState,
+                                      initialUserState,
+                                      initialNftState
+                                      }}>
         <Landing/>
       </DataContext.Provider>
     </div>
