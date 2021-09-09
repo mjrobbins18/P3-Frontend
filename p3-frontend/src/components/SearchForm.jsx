bf6043081635aa9f153e98c7ec075c0331dddc1e
mjrobbins18 <maxrobbins88@gmail.com>
2021-09-08 20:50:07 -0400

    backend and frontend working and deployed

--- p3-frontend/src/components/SearchForm.jsx
+++ p3-frontend/src/components/SearchForm.jsx
@@ -1,29 +1,44 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { useHistory } from 'react-router';
 import { DataContext } from './DataContext';
 import searchIcon from './Images/searchIcon.png'
 
 function SearchForm(props) {
 
-const {searchState, setSearchState, formState, setFormState} = useContext(DataContext)
+const {searchState, setSearchState, formState, setFormState, setRandom} = useContext(DataContext)
 const history = useHistory()
+let url = "https://nft-api-p3.herokuapp.com/nftmarketplace"
+const [currentURL, setCurrentURL] = useState(url)
+
 
 const handleSubmit = (event) => {
-
+    event.preventDefault()
+    getNFT(searchState)
+    setSearchState("")
+    history.push('/browse')     
+ 
 }
 const handleChange = (event) => {
+    setSearchState(event.target.value)
+    setCurrentURL(url.concat(`/name/${event.target.value}`))
 
 }
-
+function getNFT() {
+    fetch(currentURL).then(res => res.json().then(res => {setRandom(res)}))
+}
 
 
     return (
         <div >
-           <form className = "search">
+           <form className = "search" onSubmit={handleSubmit}>
                
-               <input className = "searchBar" type = "text" placeholder = "Explore"/>
+               <input onChange = { handleChange }
+                      className = "searchBar"
+                      value = {searchState} 
+                      type = "text" 
+                      placeholder = "Explore"/>
                <div>
-                    <img className = "searchIcon" src = { searchIcon } alt = "searchIcon"/>
+                    <img className = "searchIcon" src = { searchIcon } alt = "searchIcon" type = "submit" onSubmit= { handleSubmit }/>
                 </div> 
               
            </form>
